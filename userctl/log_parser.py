5c4a75b08d6692016c7b7e3297545bf301560e9b
Alex Bennion <alex.bennion@gmail.com>
2020-11-20 02:24:55 -0600

    backing up

--- userctl/log_parser.py
+++ userctl/log_parser.py
@@ -35,12 +35,8 @@ class LogParser(object):
         Returns the initial state of the log summary.
         """
         return {
-            'stats': {
-                'is_traffic_alerting': False
-                # 'traffic_alert_datetime': None,
-                # 'traffic_alert_threshold': 10,
-                # 'traffic_alert_duraction_seconds': 120
-            },
+            'is_traffic_alerting': False,
+            'stats_reported_on': None,
             'rows': {}
         }
 
@@ -59,26 +55,26 @@ class LogParser(object):
         """
         return int(row[col])
 
-    def get_event_time(self, timestamp, *args, **kwargs):
+    def get_event_date(self, timestamp, *args, **kwargs):
         """
         Returns the event datetime of the log entry based on the timestamp.
         """
         return datetime.fromtimestamp(timestamp)
 
-    def update_state(self, state, event_time, section, *args, **kwargs):
+    def update_state(self, state, event_date, section, *args, **kwargs):
         """
         Updates the state of the log summary.
         """
-        # event_time / section: count
-        if not event_time in state['rows'].keys():
-            state['rows'][event_time] = {
+        # event_date / section: count
+        if not event_date in state['rows'].keys():
+            state['rows'][event_date] = {
                 section: 1
             }
         else:
-            if not section in state['rows'][event_time].keys():
-                state['rows'][event_time][section] = 1
+            if not section in state['rows'][event_date].keys():
+                state['rows'][event_date][section] = 1
             else:
-                state['rows'][event_time][section] += 1
+                state['rows'][event_date][section] += 1
         return state
 
     def remove_old_rows(self, state, from_datetime, *args, **kwargs):
@@ -95,12 +91,12 @@ class LogParser(object):
         Returns the total summary count of requests.
         """
         total_requests = 0
-        for event_time in self.state['rows']:
-            for section in self.state['rows'][event_time]:
-                total_requests += self.state['rows'][event_time][section]
+        for event_date in self.state['rows']:
+            for section in self.state['rows'][event_date]:
+                total_requests += self.state['rows'][event_date][section]
         return total_requests
 
-    def check_traffic(self, event_time, *args, **kwargs):
+    def check_traffic(self, state, event_date, *args, **kwargs):
         # NOTE: Whenever total traffic for the past 2 minutes exceeds a certain
         # number on average, print a message to the console saying that “High
         # traffic generated an alert - hits = {value}, triggered at {time}”.
@@ -110,33 +106,62 @@ class LogParser(object):
         total_requests = self.get_total_requests()
         if ((total_requests > self.traffic_alert_minutes * 60 *
                 self.traffic_alert_requests_per_minute)
-                and not self.state['stats']['is_traffic_alerting']):
-            self.state['stats']['is_traffic_alerting'] = True
+                and not state['is_traffic_alerting']):
+            state['is_traffic_alerting'] = True
             msg = 'High traffic generated an alert - hits = {}, triggered at {}'
-            print(msg.format(total_requests, event_time))
+            print(msg.format(total_requests, event_date))
         elif ((total_requests <= self.traffic_alert_minutes * 60 *
                 self.traffic_alert_requests_per_minute)
-                and self.state['stats']['is_traffic_alerting']):
-            self.state['stats']['is_traffic_alerting'] = False
+                and state['is_traffic_alerting']):
+            state['is_traffic_alerting'] = False
             msg = 'High traffic alert recovered - hits = {}, triggered at {}'
-            print(msg.format(total_requests, event_time))
+            print(msg.format(total_requests, event_date))
+        return state
 
-    def check_stats(self, from_datetime, *args, **kwargs):
+    def check_stats(self, state, event_date, *args, **kwargs):
         # NOTE: For every 10 seconds of log lines, display stats about the traffic
         # during those 10 seconds: the sections of the web site with the most
         # hits.
+
+        # if we haven't checked stats before, set a value and return state
+        if state['stats_reported_on'] is None:
+            state['stats_reported_on'] = event_date
+            return state
+
+        # print('test3')
+
+        from_date = event_date - timedelta(
+            seconds=self.stats_seconds)
+
+        # print('test4', state['stats_reported_on'])
+
+        # if event_date - stats_reported_on is less than 10 seconds, exit
+        if state['stats_reported_on'] > from_date:
+            # print('test5')
+            return state
+
+        # print('test6')
+
+        # filter out rows older than the from date
         stats_rows = {
-            k: v for (k, v) in self.state['rows'].items()
-            if k >= from_datetime
+            k: v for (k, v) in state['rows'].items()
+            if k >= from_date
         }
 
+        # else, has it been 10 seconds? we need the new max datetime?
+
         stats = {}
-        for event_time in stats_rows.values():
-            for section, count in event_time.items():
+        for value in stats_rows.values():
+            for section, count in value.items():
                 if not section in stats:
                     stats[section] = 0
                 stats[section] += count
-        print('stats', stats)
+
+        print('stats', event_date, stats)
+
+        state['stats_reported_on'] = event_date
+
+        return state
 
     def get_row(self, line, *args, **kwargs):
         # TODO: Create factory to handle other types of logs.
@@ -152,18 +177,21 @@ class LogParser(object):
         log.debug('line: %s', line)
         timestamp_column = kwargs.get('timestamp_column', 3)
         section_column = kwargs.get('section_column', 4)
+
         row = self.get_row(line, *args, **kwargs)
         timestamp = self.get_timestamp(row, timestamp_column)
-        event_time = self.get_event_time(timestamp)
-        stats_from_date = event_time - timedelta(
-            seconds=self.stats_seconds)
-        alert_from_date = event_time - timedelta(
-            seconds=self.traffic_alert_minutes * 60)
+        event_date = self.get_event_date(timestamp)
         section = self.get_section(row, section_column)
-        self.state = self.update_state(self.state, event_time, section)
+
+        self.state = self.update_state(self.state, event_date, section)
+
+        alert_from_date = event_date - timedelta(
+            seconds=self.traffic_alert_minutes * 60)
+
         self.state = self.remove_old_rows(self.state, alert_from_date)
-        self.check_traffic(event_time)
-        self.check_stats(stats_from_date)
+        self.state = self.check_traffic(self.state, event_date)
+        self.state = self.check_stats(self.state, event_date)
+
         return self.state
 
     def get_input(self, *args, **kwargs):
